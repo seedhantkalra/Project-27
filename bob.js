06f0654704cff1b60837d5b74606e066458eb094
seedhantkalra <70706855+seedhantkalra@users.noreply.github.com>
2021-02-01 21:12:02 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,33 @@
+class Bob {
+    constructor(x,y,r) {
+        var options={
+            isStatic: false ,
+            restitution: 0.3, 
+            friction: 0.5,
+            density:1.2 
+
+        }
+        
+        this.body=Bodies.circle(x,y,r/2, options);
+        this.x=x;
+        this.y=y;
+        this.r=r
+        World.add(world, this.body);    
+    } 
+
+display(){
+
+  var paperpos= this.body.position;
+var angle = this.body.angle;        
+  push();
+  translate(paperpos.x, paperpos.y);
+  rotate(angle);
+  ellipseMode(CENTER);
+  strokeWeight(3);
+  fill(255,0,255)
+  ellipse(0,0,this.r,this.r);
+  pop() 
+}
+
+
+}
